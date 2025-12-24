ba8962a387094f63ab23bab343f9c980e0df0429
Geoffrey Jensen <geoffrey.jensen@cogeco.ca>
2025-12-24 12:13:00 -0500

    initial cnn implementation. convolutional layer learning needs fixing

---
+++ ConvolutionalNetwork.py
@@ -0,0 +1,74 @@
+# COSC 4P80 Project
+# Geoffrey Jensen       7148710
+# Stephen Stefanidis    7140030
+# Nicholas Parise       7242530
+#
+# Convolutional Network Class
+
+from ConvolutionalLayer import *
+from FullyConnectedLayer import *
+import numpy as np
+
+class ConvolutionalNetwork:
+
+    def __init__(self, config):
+        assert 'learning_rate' in config, "The config must provide a value for \'learning_rate\'"
+        self.learning_rate = config['learning_rate']
+
+        assert 'convolutional_layers' in config, "The config must provide an array for \'convolutional_layers\'"
+        assert len(config['convolutional_layers']) >= 1, "\'convolutional_layers\' must not be empty"
+        
+        prev_nodes = 28     # number of nodes in one dimension
+        kernel_z = 1        # depth of kernel. Starts as 1 for the single channel image
+        self.convolutional_layers = []
+        for conv_layer in config['convolutional_layers']:
+            self.convolutional_layers.append(ConvolutionalLayer(conv_layer, self.learning_rate, kernel_z))
+            kernel_z = conv_layer['kernel_count']
+            prev_nodes = prev_nodes - conv_layer['kernel_dim'] - conv_layer['padding'] + 1
+            prev_nodes = prev_nodes // conv_layer['pooling_dim']
+        self.convolutional_layers[0].set_first_layer(True)
+
+        self.last_convolutional_shape = (kernel_z, prev_nodes, prev_nodes)
+        prev_nodes = kernel_z * prev_nodes**2
+        self.fully_connected_layers = []
+        if 'fully_connected_layers' in config and len(config['fully_connected_layers']) > 0:
+            for fc_layer in config['fully_connected_layers']:
+                assert 'node_count' in fc_layer, "The config must provide a value for \'node_count\' in each fully connected layer definition"
+                self.fully_connected_layers.append(FullyConnectedLayer(self.learning_rate, prev_nodes, fc_layer['node_count'], hidden=True))
+                prev_nodes = fc_layer['node_count']
+
+        self.output_layer = FullyConnectedLayer(self.learning_rate, prev_nodes, 10, hidden=False) # add output layer
+
+    # Feed a sample x through the network, and apply backpropagation learning
+    def apply_and_learn(self, x, y):
+        o = self.apply(x)
+        self.__learn(y)
+        return o
+
+    def apply(self, x):
+        channels = np.array([x]) # z, x, y
+        # feed data through convolutional layers
+        for conv_layer in self.convolutional_layers:
+            channels = conv_layer.apply(channels)
+
+        input_vector = channels.flatten()
+        for layer in self.fully_connected_layers:
+            input_vector = layer.apply(input_vector)
+
+        return self.output_layer.apply(input_vector)
+
+    # Apply backpropagation learning. Must be used after apply(x).
+    def __learn(self, y):
+        prop_error = self.output_layer.learn_output(y)
+
+        for layer in reversed(self.fully_connected_layers):
+            prop_error = layer.learn_hidden(prop_error)
+
+        prop_error = prop_error.reshape(self.last_convolutional_shape)
+
+        for layer in reversed(self.convolutional_layers):
+            prop_error = layer.learn(prop_error)
+
+
+        
+
---
+++ FullyConnectedLayer.py
@@ -0,0 +1,120 @@
+# COSC 4P80 Project
+# Geoffrey Jensen       7148710
+# Stephen Stefanidis    7140030
+# Nicholas Parise       7242530
+#
+# Fully Connected Layer Class
+
+import random
+import numpy as np
+
+# Params for random initialization
+RANGE = 0.5
+BASE = -0.25
+LOWEST_VAL = 0.001
+
+class FullyConnectedLayer:
+    def __init__(self, learning_rate, input_nodes, output_nodes, hidden):
+        self.learning_rate = learning_rate
+        self.weights = np.empty([input_nodes, output_nodes])  # nodes[input_node][output_node]
+        self.biases = np.empty(output_nodes)
+        self.error = np.empty(output_nodes)
+        self.hidden = hidden
+
+        # randomly initialize weights
+        for i in range(len(self.weights)):
+            for j in range(len(self.weights[0])):
+                val = random.random() * RANGE + BASE
+                while abs(val) < LOWEST_VAL:     # Guarentee starting weights are not too small
+                    val = random.random() * RANGE + BASE
+
+                self.weights[i][j] = val
+
+        # randomly initialize biases
+        for i in range(len(self.biases)):
+            val = random.random() * RANGE + BASE
+            while val < LOWEST_VAL:     # Guarentee starting biases are not too small
+                val = random.random() * RANGE + BASE
+            self.biases[i] = val
+
+    # Feed an input vector x through this layer and return output vector
+    def apply(self, x):
+        self.output = np.empty(len(self.weights[0]))
+        self.input = x
+        for i in range(len(self.weights[0])):
+            self.output[i] = self.__evaluate_node(i, x)
+
+        if not self.hidden: # output layer, apply softmax
+            self.output = self.softmax(self.output)
+            
+        return self.output
+
+    # Evaluate a single neuron given input vector x
+    def __evaluate_node(self, node, x):
+        res = 0
+        for i in range(len(x)):
+            res += x[i] * self.weights[i][node]   # sum(input * weight)
+        
+        return self.__activate(res + self.biases[node])
+
+    # activation function (sigmoid)
+    def __activate(self, x):
+        return 1 / (1 + np.exp(-x)) if x > -600 else 0  # avoid integer overflow from np.exp()
+
+    # Adjust weights and biases of this Layer based on the current self.error values
+    def __adjust_layer(self):
+        self.weights -= self.learning_rate * np.outer(self.input, self.error)
+        self.biases -= self.learning_rate * self.error
+
+    # Backprop learning for the output layer
+    # Based on slide 77 of Backpropagation slides
+    def learn_output(self, expected_val):
+        assert not self.hidden, "Trying to apply BP on the output layer with a hidden layer"
+        
+        expected_output = [0.0 for _ in range(len(self.output))]
+        expected_output[expected_val] = 1.0
+
+        # For Mean Squared Error (MSE)
+        # derivative of sigmoid * derivative of mean squared error
+        # self.error = self.output * (1 - self.output) * (self.output - expected_output)
+
+        # For Categorical Cross-Entropy
+        # The derivative of sigmoid cancels out the denominator of Cross-Entropy derivative, simplifying down to y_hat - y
+        self.error = self.output - expected_output
+            
+        next_error = np.zeros(len(self.weights))
+        for i in range(len(self.weights)):
+            next_error[i] = np.dot(self.weights[i], self.error)
+
+        self.__adjust_layer()
+            
+        return next_error
+
+    # Backprop learning for hidden layers
+    # Based on slide 77 of Backpropagation slides
+    def learn_hidden(self, prop_error):
+        assert self.hidden, "Trying to apply BP on a hidden layer with an output layer"
+
+        self.error = self.output * (1 - self.output) * prop_error
+
+        next_error = np.zeros(len(self.weights))
+
+        for i in range(len(self.weights)):
+            next_error[i] = np.dot(self.weights[i], self.error)
+
+        self.__adjust_layer()
+
+        return next_error
+
+    # convert data vector to softmax vector
+    def softmax(self, data):
+        output = np.empty(len(data))
+
+        denominator = 0
+        for val in data:    # calculate denominator
+            denominator += np.exp(val)
+
+        for i, val in enumerate(data):
+            output[i] = np.exp(val) / denominator
+
+        return output
